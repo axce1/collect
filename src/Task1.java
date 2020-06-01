1624b26cb855e861d13d1f7d927495dbb9bdbd58
axcel <axcel.github@gmail.com>
2020-06-01 06:56:01 +0500

    task 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
+class CustomCollection<Integer> {
+
+    private int size;
+    Integer[] elementData;
+
+    public CustomCollection() {
+
+    }
+
+    private Boolean checkElement(Object o) {
+        return o != null;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+}
+
 public class Task1 {
     public static void main(String[] args) {
 
